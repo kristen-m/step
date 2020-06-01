db228c6d03c7bc3d2db36af828f20d8ccecbb6a9
Kristen <kmason2013@scrippscollege.edu>
2020-06-01 11:33:07 -0500

    Hardcode greeting

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -25,6 +25,6 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Kristen!</h1>");
+    response.getWriter().println("Hello Kristen!");
   }
 }
