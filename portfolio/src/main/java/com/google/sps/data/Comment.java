3311c62dbd2b80115ea9b472c4e2e02638326594
Kristen <kmason2013@scrippscollege.edu>
2020-06-05 22:16:04 +0000

    Add additional variable to class

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -5,11 +5,13 @@ public class Comment {
   private String content;
   private Comment parent;
   private String postTime;
+  private long id;
 
-  public Comment(String content, String postTime, Comment parent) {
+  public Comment(String content, String postTime, Comment parent, long id) {
     this.content = content;
     this.postTime = postTime;
     this.parent = parent;
+    this.id = id;
   }
 
   public String getContent() {
@@ -22,6 +24,14 @@ public class Comment {
 
   public boolean isReply() {
     return this.parent!=null;
+  }
+
+  public long getId(){
+    return this.id;
+  }
+
+  public void setId(long newId) {
+    this.id = newId;
   }
 
   public void setParent(Comment p) {
