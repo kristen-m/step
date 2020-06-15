d1846be3cc8019336c9975188ff901fe72888998
Kristen <kmason2013@scrippscollege.edu>
2020-06-15 20:37:44 +0000

    Log unavailable times

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -25,6 +25,7 @@ import java.util.Set;
 
 
 public final class FindMeetingQuery {
+  private static int DAY_LENGTH = 24*60;
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     List<TimeRange> possibleTimes = new ArrayList<TimeRange>();
     Set<String> attendees =  new HashSet<String>();
@@ -33,7 +34,7 @@ public final class FindMeetingQuery {
     attendees.addAll(request.getAttendees());
 
     //If the meeting duration is greater than 24 hours return no options
-    if (meetingDur > 24*60){
+    if (meetingDur > DAY_LENGTH){
       return possibleTimes;
     }
 
@@ -42,7 +43,37 @@ public final class FindMeetingQuery {
       possibleTimes.add(TimeRange.WHOLE_DAY);
       return possibleTimes;
     }
-    
+
+    List<TimeRange> busyTimes = new ArrayList<TimeRange>();
+    for(Event event : events) {
+      Set<String> eventAttendees = event.getAttendees();
+      Set<String> overlap = new HashSet<>(eventAttendees);
+      overlap.retainAll(attendees);
+      if (!overlap.isEmpty()){
+        busyTimes.add(event.getWhen());
+      }
+    }
+    Collections.sort(busyTimes, TimeRange.ORDER_BY_START);
+    System.out.println(busyTimes);  
+    TimeRange[] unavailable = ((List<TimeRange>) busyTimes).toArray(new TimeRange[busyTimes.size()]); 
+
+    int index = 0;
+    for (int i=0; i < unavailable.length; i++) {
+      int startTime = 0;
+      int endTime = 0;
+      if(i == 0) {
+        startTime = 0;
+        endTime = unavailable[i].start();
+      }
+      else{
+        startTime = unavailable[index].start();
+        endTime = unavailable[i].end();
+      }
+      TimeRange freeTime = TimeRange.fromStartEnd(startTime,endTime,false);
+      possibleTimes.add(freeTime);
+      index++;
+    }
+    System.out.println(possibleTimes);
     return possibleTimes;
   }
 }
